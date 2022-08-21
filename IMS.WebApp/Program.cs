9a950e843b85b4cb8078fe9b0edb6a7eb185a25d
Tim Franke <tfranke0@gmail.com>
2022-08-21 14:55:12 -0500

    Product page and components completed.

--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -2,6 +2,8 @@ using IMS.Plugins.InMemory;
 using IMS.UseCases.Inventories;
 using IMS.UseCases.Inventories.Interfaces;
 using IMS.UseCases.PluginInterfaces;
+using IMS.UseCases.Products;
+using IMS.UseCases.Products.Interfaces;
 using IMS.WebApp.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -15,11 +17,18 @@ builder.Services.AddSingleton<WeatherForecastService>();
 
 //Singleton gives same copy to each user. 1 instance for application
 builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
+builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+
 //Transient give each user it's own copy. 1 instance per user.
 builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
 builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase>();
 builder.Services.AddTransient<IUpdateInventoryUseCase, UpdateInventoryUseCase>();
 builder.Services.AddTransient<IViewInventoryByIdUseCase, ViewInventoryByIdUseCase>();
+
+builder.Services.AddTransient<IViewProductsByNameUseCase, ViewProductsByNameUseCase>();
+builder.Services.AddTransient<IAddProductUseCase, AddProductUseCase>();
+builder.Services.AddTransient<IViewProductByIdUseCase, ViewProductByIdUseCase>();
+builder.Services.AddTransient<IUpdateProductUseCase, UpdateProductUseCase>();
 //Scoped give each user it's own copy, but lifetime is for object's instance's lifetime.
 //i.e. for SignalR, when you refresh the browser and disconnect and then get a new SignalR connection,
 //you get a new copy of this instanced object.
